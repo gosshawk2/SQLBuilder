c68c7caafb34480086e89b53ddfb2fbee13c96c3
Daniel Goss <gosshawk2@yahoo.co.uk>
2020-04-06 13:53:59 +0100

    SQLBuilder v1.0.1.1 06-04-2020 13:55 Changes made by GL to ViewSQL = Run button added with code to execute query in IBMi DB2 series database and Imports excel = Microsoft.Office.Interop.Excel was added to the same form. GL.

--- SQLBuilder/ViewSQL.Designer.vb
+++ SQLBuilder/ViewSQL.Designer.vb
@@ -26,13 +26,21 @@ Partial Class ViewSQL
         Me.txtSQLQuery = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnClose = New System.Windows.Forms.Button()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.dgv = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.tssLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label4
         '
         Me.Label4.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(-91, -89)
+        Me.Label4.Location = New System.Drawing.Point(-91, 224)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(62, 13)
         Me.Label4.TabIndex = 11
@@ -46,7 +54,7 @@ Partial Class ViewSQL
         Me.txtSQLQuery.Multiline = True
         Me.txtSQLQuery.Name = "txtSQLQuery"
         Me.txtSQLQuery.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.txtSQLQuery.Size = New System.Drawing.Size(849, 115)
+        Me.txtSQLQuery.Size = New System.Drawing.Size(1028, 90)
         Me.txtSQLQuery.TabIndex = 10
         '
         'Label1
@@ -54,30 +62,84 @@ Partial Class ViewSQL
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(12, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.Size = New System.Drawing.Size(81, 13)
         Me.Label1.TabIndex = 12
-        Me.Label1.Text = "Final Query:"
+        Me.Label1.Text = "Generated SQL"
         '
         'btnClose
         '
-        Me.btnClose.Location = New System.Drawing.Point(15, 152)
+        Me.btnClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnClose.Location = New System.Drawing.Point(12, 441)
         Me.btnClose.Name = "btnClose"
         Me.btnClose.Size = New System.Drawing.Size(75, 23)
         Me.btnClose.TabIndex = 13
         Me.btnClose.Text = "Close"
         Me.btnClose.UseVisualStyleBackColor = True
         '
+        'btnRun
+        '
+        Me.btnRun.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnRun.Location = New System.Drawing.Point(108, 441)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(75, 23)
+        Me.btnRun.TabIndex = 14
+        Me.btnRun.Text = "Run"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'dgv
+        '
+        Me.dgv.AllowUserToAddRows = False
+        Me.dgv.AllowUserToDeleteRows = False
+        Me.dgv.AllowUserToOrderColumns = True
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgv.Location = New System.Drawing.Point(3, 16)
+        Me.dgv.Name = "dgv"
+        Me.dgv.ReadOnly = True
+        Me.dgv.Size = New System.Drawing.Size(1019, 275)
+        Me.dgv.TabIndex = 15
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.dgv)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 123)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1025, 294)
+        Me.GroupBox1.TabIndex = 16
+        Me.GroupBox1.TabStop = False
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tssLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 480)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(1055, 22)
+        Me.StatusStrip1.TabIndex = 17
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'tssLabel1
+        '
+        Me.tssLabel1.Name = "tssLabel1"
+        Me.tssLabel1.Size = New System.Drawing.Size(0, 17)
+        '
         'ViewSQL
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(876, 189)
+        Me.ClientSize = New System.Drawing.Size(1055, 502)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnRun)
         Me.Controls.Add(Me.btnClose)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.txtSQLQuery)
         Me.Name = "ViewSQL"
         Me.Text = "ViewSQL"
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -87,4 +149,9 @@ Partial Class ViewSQL
     Friend WithEvents txtSQLQuery As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents btnClose As Button
+    Friend WithEvents btnRun As Button
+    Friend WithEvents dgv As DataGridView
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents tssLabel1 As ToolStripStatusLabel
 End Class
--- SQLBuilder/ViewSQL.vb
+++ SQLBuilder/ViewSQL.vb
@@ -1,14 +1,57 @@
-﻿Public Class ViewSQL
+﻿Imports excel = Microsoft.Office.Interop.Excel
+Imports System.Data.Odbc
+Public Class ViewSQL
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnClose.Click
         Close()
 
     End Sub
 
     Private Sub ViewSQL_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        '
+
     End Sub
 
     Sub PopulateForm(SQLQuery As String)
         txtSQLQuery.Text = SQLQuery
     End Sub
+    Public Function ExecuteSQLQuery(SQLStatement As String) As DataTable
+        Dim ConnectString As String
+        ConnectString = "Provider=MSDASQL.1;DRIVER=Client Access ODBC Driver (32-bit);SYSTEM=PARAGON;TRANSLATE=1;DBQ=,epobespliv,epobesiliv, ault2f3,ault1f3,epocrmfliv,epoapefliv,epoutility,islrtgf,aulamf3,eposysfliv,zxref;DFTPKGLIB=QGPL;LANGUAGEID=ENU;PKG=QGPL/DEFAULT(IBM),2,0,1,0,512;LIBVIEW=1;CONNTYPE=0;userid=odbcuser;password=odbcuser;Initial Catalog=PARAGON;NAM=1 "
+        Dim cn As New OdbcConnection(ConnectString)
+        Dim cm As OdbcCommand = cn.CreateCommand 'Create a command object via the connection
+        cn.Open()
+        cm.CommandTimeout = 0
+        cm.CommandType = CommandType.Text
+        cm.CommandText = SQLStatement
+        Dim da As New OdbcDataAdapter(cm)
+        Dim ds As New DataSet
+        da.Fill(ds)
+        Return ds.Tables(0)
+    End Function
+
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles btnRun.Click
+        Dim dt As New DataTable
+        dgv.DataSource = Nothing
+        Refresh()
+        tssLabel1.Text = "Getting Data"
+        Refresh()
+        dt = ExecuteSQLQuery(txtSQLQuery.Text)
+        If dt.Rows.Count = 0 Then
+            MsgBox("No records")
+        Else
+            tssLabel1.Text = "Loading Data to Grid"
+            Refresh()
+            dgv.DataSource = dt
+            tssLabel1.Text = "Formatting Grid"
+            Refresh()
+            dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        End If
+        tssLabel1.Text = "Records:" & dt.Rows.Count
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgv.CellContentClick
+
+    End Sub
+
 End Class
